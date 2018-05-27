ba9c7f60935a7eb4f76a8c344c2dd5cba56733c6
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-27 19:24:16 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,7 +14,7 @@ class Artist
     @@all
   end
 
-  def add_song(song, genre)
+  def add_song(song)
     song.artist = self unless song.artist == self
     @songs << song unless @songs.include?(song)
   end
